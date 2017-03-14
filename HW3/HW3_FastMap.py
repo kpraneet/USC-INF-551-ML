c434f9feddfbb17a66de48710bf5bae770391932
Karthik Rao <raokarthik74@gmail.com>
2017-03-13 19:40:22 -0700

    initial commit of perceptron

---
+++ HW3/HW3_FastMap.py
@@ -0,0 +1,23 @@
+def choose_distant_objects(data_set):
+    import random
+    o_b = random.randomint(0, len(data_set))
+    #select object that is fartherest from o_b
+
+
+def fast_map(k):
+    if k <= 0:
+        return
+    else:
+        col += 1
+    #choose pivot objects
+
+def main():
+    # global variables
+    global col
+    col = 0
+    global x
+
+
+
+if __name__ == '__main__':
+    main()
