a1bdd60ccd1cef0cce1dc4db921e6c67dad9eaee
Praneet Kalluri <praneet97@gmail.com>
2017-03-14 22:50:18 -0700

    Pocket algorithm and Perceptron

--- HW4/pocket_algorithm.py
+++ HW4/pocket_algorithm.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def checkconstraints(val,label):
     count = 0
@@ -10,7 +11,7 @@ def checkconstraints(val,label):
             count+=1
         else:
             pending+=1
-    return count
+    return count,pending
 
 def main():
     data = []
@@ -18,9 +19,14 @@ def main():
     tmp = np.random.rand(3)
     thr = 0.01
     w = [thr,tmp[0],tmp[1],tmp[2]]
-    n = 10000
+    n = 7000
     eta = 0.01
     totalviolated = 0
+    xlist = []
+    ylist = []
+    pa_count = 0
+    pa_w = []
+    pa_i = 0
     print('Randomly initialized weights: ')
     print(w[0],',',w[1],',',w[2],',',w[3])
     inputfile = open("classification.txt", "r")
@@ -32,12 +38,18 @@ def main():
         templst.append(float(val.split(',')[1]))
         templst.append(float(val.split(',')[2]))
         data.append(templst)
-        label.append(int(val.split(',')[3]))
+        label.append(int(val.split(',')[4]))
     data=np.array(data)
     w=np.array(w)
     for i in range(n):
         val=np.dot(data,w)
-        count = checkconstraints(val,label)
+        count,pending = checkconstraints(val,label)
+        xlist.append(i)
+        ylist.append(pending)
+        if count > pa_count:
+            pa_count = count
+            pa_w = w
+            pa_i = i
         if count==2000:
             break
         else:
@@ -51,12 +63,16 @@ def main():
                     w -= tmpval
                 else:
                     totalviolated += 1
-    print('Final weights: ')
-    print(w[0],',',w[1],',',w[2],',',w[3])
-    print('Number of constraints satisfied: ')
-    print(count)
-    print('Number of iterations: ')
-    print(i)
+    print('Weights where we had least constraints violated: ')
+    print(pa_w[0],',',pa_w[1],',',pa_w[2],',',pa_w[3])
+    print('Least amount of constraints violated: ')
+    print(2000-pa_count)
+    print('Iteration where least number of constraints were violated: ')
+    print(pa_i)
+    plt.plot(xlist,ylist)
+    plt.xlabel('Number of Iterations')
+    plt.ylabel('Violated constraints')
+    plt.show()
 
 if __name__ == '__main__':
     main()
