41bd7ebbe827e5fa67610c311f5171be7e6dfa03
Praneet Kalluri <praneet97@gmail.com>
2017-03-15 21:01:16 -0700

    Updated files

--- HW4/linear_regression.py
+++ HW4/linear_regression.py
@@ -1,31 +1,24 @@
-# Code source: Jaques Grobler
-# License: BSD 3 clause
-
-
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn import datasets, linear_model
-import pandas as pd
 
 def main():
+    data = []
+    zlist = []
     inputfile = open("linear-regression.txt", "r")
     filecontent = inputfile.read().splitlines()
-    label = []
-    data = []
     for val in filecontent:
-        templst = []
-        templst.append(float(val.split(',')[0]))
-        templst.append(float(val.split(',')[1]))
-        label.append(float(val.split(',')[2]))
-        data.append(templst)
-    # Create linear regression object
-    regr = linear_model.LinearRegression()
-
-    # Train the model using the training sets
-    regr.fit(data, label)
-
-    # The coefficients
-    print('Coefficients: \n', regr.coef_)
+        tmplist = []
+        tmplist.append(float(1.0))
+        tmplist.append(float(val.split(',')[0]))
+        tmplist.append(float(val.split(',')[1]))
+        zlist.append(float(val.split(',')[2]))
+        data.append(tmplist)
+    data = np.array(data)
+    zlist = np.array(zlist)
+    tmpdata = np.linalg.inv(np.dot((data.T),data))
+    updateddata = np.dot(tmpdata,(data.T))
+    w = np.dot(updateddata,zlist)
+    print('Weights: ')
+    print(w[0],',',w[1],',',w[2])
 
 if __name__ == '__main__':
     main()
--- HW4/perceptron.py
+++ HW4/perceptron.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 
 def checkconstraints(val,label):
     count = 0
@@ -11,7 +10,7 @@ def checkconstraints(val,label):
             count+=1
         else:
             pending+=1
-    return count,pending
+    return count
 
 def main():
     data = []
@@ -22,11 +21,6 @@ def main():
     n = 7000
     eta = 0.01
     totalviolated = 0
-    xlist = []
-    ylist = []
-    pa_count = 0
-    pa_w = []
-    pa_i = 0
     print('Randomly initialized weights: ')
     print(w[0],',',w[1],',',w[2],',',w[3])
     inputfile = open("classification.txt", "r")
@@ -38,20 +32,14 @@ def main():
         templst.append(float(val.split(',')[1]))
         templst.append(float(val.split(',')[2]))
         data.append(templst)
-        label.append(int(val.split(',')[4]))
+        label.append(int(val.split(',')[3]))
     data=np.array(data)
     w=np.array(w)
     for i in range(n):
         val = []
         for x in range(len(data)):
-            val.append(data[x][0] * w[0] + data[x][1] * w[1] + data[x][2] * w[2] + data[x][3] * w[3])
-        count,pending = checkconstraints(val,label)
-        xlist.append(i)
-        ylist.append(pending)
-        if count > pa_count:
-            pa_count = count
-            pa_w = w
-            pa_i = i
+            val.append(data[x][0]*w[0]+data[x][1]*w[1]+data[x][2]*w[2]+data[x][3]*w[3])
+        count = checkconstraints(val,label)
         if count==2000:
             break
         else:
@@ -69,16 +57,33 @@ def main():
                     w[3] = w[3] - (eta * indvalarray[3])
                 else:
                     totalviolated += 1
-    print('Weights where we had least constraints violated: ')
-    print(pa_w[0],',',pa_w[1],',',pa_w[2],',',pa_w[3])
-    print('Least amount of constraints violated: ')
-    print(2000-pa_count)
-    print('Iteration where least number of constraints were violated: ')
-    print(pa_i)
-    plt.plot(xlist,ylist)
-    plt.xlabel('Number of Iterations')
-    plt.ylabel('Violated constraints')
-    plt.show()
+    print('Final weights: ')
+    print(w[0],',',w[1],',',w[2],',',w[3])
+    print('Number of constraints satisfied: ')
+    print(count)
+    print('Number of iterations: ')
+    print(i)
+    # mlabel = 0
+    # plabel = 0
+    # for x in range(len(data)):
+    #     indvalarray = np.array(data[x])
+    #     if((np.dot(indvalarray,w)<0.0) and label[x]==-1):
+    #         mlabel+=1
+    #     elif((np.dot(indvalarray,w)>=0.0) and label[x]==1):
+    #         plabel+=1
+    #     else:
+    #         print('Misclassification!')
+    # print(plabel,mlabel)
+    # labp = 0
+    # labm = 0
+    # for x in label:
+    #     if x==1:
+    #         labp+=1
+    #     elif x==-1:
+    #         labm+=1
+    #     else:
+    #         print('Other tag')
+    # print(labp,labm)
 
 if __name__ == '__main__':
     main()
