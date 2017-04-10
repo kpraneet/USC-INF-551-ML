083a912a00c862f77278fdbb61bc0f70d53abca1
Karthik Rao <raokarthik74@gmail.com>
2017-04-10 02:40:58 -0700

    working commit

--- HW6/viterbi.py
+++ HW6/viterbi.py
@@ -1,5 +1,60 @@
 import numpy as np
 from scipy.spatial import distance
+from decimal import *
+
+#calculates the distance from all the towers
+def distance_from_towers(x,y):
+    coords = [(x, y), (0,0), (0,9), (9,0), (9,9)]
+    distance_array =  np.array(distance.cdist(coords, coords, 'euclidean'))[0]
+    distance_array = np.delete(distance_array, 0)
+    return distance_array
+
+#function to prepare the emission probability matrix - given a state what is the probability of observation
+def prepare_emission_probability(states_array, observations):
+    emission_prob = [{}]
+    for count,observation in enumerate(observations):
+        temp_list = []
+        for state in states_array:
+            distance_array = distance_from_towers(state[0], state[1])
+            if (observation.item(0) >= distance_array[0]*0.7 and observation.item(0) <= distance_array[0]*1.3) and (observation.item(1) >= distance_array[1]*0.7 and observation.item(1) <= distance_array[1]*1.3) and (observation.item(2) >= distance_array[2]*0.7 and observation.item(2) <= distance_array[2]*1.3) and (observation.item(3) >= distance_array[3]*0.7 and observation.item(3) <= distance_array[3]*1.3):
+                temp_list.append(state)
+        temp_emission_probability = 1/float(len(temp_list))
+        emission_prob.append({})
+        for temp in temp_list:
+            emission_prob[count][temp] = temp_emission_probability
+    print (emission_prob)
+    return emission_prob
+
+# function to prepare the transition probability matrix - the probability of transition from one state to another
+def prepare_transition_probability(states_array):
+    transition_probability = {}
+    count = 0
+    for state in states_array:
+        count = 0
+        if (state[0]+1, state[1]) in states_array:
+            count += 1
+        if (state[0], state[1]+1) in states_array:
+            count += 1
+        if (state[0]-1, state[1]) in states_array:
+            count += 1
+        if (state[0], state[1]-1) in states_array:
+            count += 1
+        temp_probability = float(1/Decimal(count))
+        temp_dictionary = {}
+        if (state[0]+1, state[1]) in states_array:
+            temp_dictionary[(state[0]+1, state[1])] = temp_probability
+        if (state[0], state[1]+1) in states_array:
+            temp_dictionary[(state[0], state[1]+1)] = temp_probability
+        if (state[0]-1, state[1]) in states_array:
+            temp_dictionary[(state[0]-1, state[1])] = temp_probability
+        if (state[0], state[1]-1) in states_array:
+            temp_dictionary[(state[0], state[1]-1)] = temp_probability
+        transition_probability[state] = temp_dictionary
+    print (transition_probability)
+    return transition_probability
+
+
+
 
 #Prepares data as mentioned in the assignment description
 def prepare_data():
@@ -9,46 +64,34 @@ def prepare_data():
     states_array = []
     for x in range(states.shape[0]):
         for y in range(states.shape[1]):
-            if states.item(x,y) != 0 and (x !=0 and y!=0) and (x !=0 and y!=9) and (x !=9 and y!=0) and (x !=9 and y!=9):
+            if states.item(x,y) != 0:
                 states_array.append((x,y))
     towers = np.matrix('0 0; 0 9; 9 0; 9 9')
-    return observations, states, towers, states_array
+    print (states_array)
+    emission_probability = prepare_emission_probability(states_array, observations)
+    transition_probability = prepare_transition_probability(states_array)
+    return observations, states_array, emission_probability, transition_probability
 
-#Performs viterbi algorithm to determine the best possible hidden state sequence for the given observations.
-def distance_from_towers(x,y):
-    coords = [(x, y), (0,0), (0,9), (9,0), (9,9)]
-    distance_array =  np.array(distance.cdist(coords, coords, 'euclidean'))[0]
-    distance_array = np.delete(distance_array, 0)
-    return distance_array
-
-def random_emission_probability(x, y):
-    array = []
-    random_factor = 0.7
-    tower_distance_array = distance_from_towers(x,y)
-    while random_factor < 1.4:
-        for x in tower_distance_array:
-            array.append(x*random_factor)
-        random_factor += 0.1
-    choice = np.random.choice(array, 1)[0]
-    return choice
-
-def viterbi(observations, states, towers, states_array):
+#Performs viterbi algorithm to determine the best possible hidden state sequence for the given observations
+def viterbi(observations, states_array, emission_probability, transition_probability):
     V = [{}]
     for st in states_array:
-        V[0][st] = {"probability": 1/100 * random_emission_probability(st[0],st[1]), "previous" : None}
-
+        V[0][st] = {"probability": float(1/Decimal(87)) * emission_probability[0].get(st, 0), "previous" : None}
     for t in range(1, len(observations)):
         V.append({})
         for st in states_array:
-            max_tr_prob = max(V[t - 1][prev_st]["probability"] * 0.25 for prev_st in states_array)
+            max_tr_prob = 0
             for prev_st in states_array:
-                if V[t - 1][prev_st]["probability"] * 0.25 == max_tr_prob:
-                    max_prob = max_tr_prob * random_emission_probability(prev_st[0],prev_st[1])
+                if V[t - 1][prev_st]["probability"] * transition_probability.get(prev_st, 0).get(st, 0) > max_tr_prob:
+                    max_tr_prob = V[t - 1][prev_st]["probability"] * transition_probability.get(prev_st, 0).get(st, 0)
+            for prev_st in states_array:
+                if V[t - 1][prev_st]["probability"] * transition_probability.get(prev_st, 0).get(st, 0) == max_tr_prob:
+                    max_prob = max_tr_prob * emission_probability[t].get(st, 0)
                     V[t][st] = {"probability": max_prob, "previous": prev_st}
                     break
-
     opt = []
     max_prob = max(value["probability"] for value in V[-1].values())
+    print (max_prob)
     previous = None
 
     for st, data in V[-1].items():
@@ -58,11 +101,16 @@ def viterbi(observations, states, towers, states_array):
 
     final_opt = []
     min_x = 10
+    min_y = 10
     min_coordinate = ()
-    #tie breaking
+    #tie breaking0
+    print (opt)
     for op in opt:
         if op[0] < min_x:
-            min = op[0]
+            min_x = op[0]
+            min_coordinate = op
+        elif op[0] == min_x and op[1] < min_y:
+            min_y = op[1]
             min_coordinate = op
 
     final_opt.append(min_coordinate)
@@ -72,11 +120,11 @@ def viterbi(observations, states, towers, states_array):
         previous = V[t + 1][previous]["previous"]
 
     print(final_opt)
-    # print ('The steps of states are ' + ' '.join(opt) + ' with highest probability of %s' % max_prob)
+    print ('The steps of states are ' + ' '.join(str(final_opt)) + ' with highest probability of %s' % max_prob)
 
 def main():
-    observations, states, towers, states_array = prepare_data()
-    viterbi(observations, states, towers, states_array)
-    random_emission_probability(1,1)
+    observations, states_array, emission_probability, transition_probability = prepare_data()
+    viterbi(observations, states_array, emission_probability, transition_probability)
+
 if __name__ == '__main__':
     main()
